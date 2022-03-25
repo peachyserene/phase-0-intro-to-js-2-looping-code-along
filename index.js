55c96443df328aef7dc25699ae9ee894ac8cdde9
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-03-25 01:18:12 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const messages = [];
+
+function writeCards(names, occasion) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+        debugger;
+    }
+    return messages;
+}
+
+function countDown(number) {
+    let startNum = number;
+    while (startNum >= 0) {
+        console.log(startNum--);
+        debugger;
+    }
+}
